afc87863201882c5248f1ba1c524553de3a83c68
hisohiso3b <hisohiso3b@gmail.com>
2015-07-22 04:52:51 +0900

    Initial Commit

---
+++ SimpleBBS/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SimpleBBS
+//
+//  Created by hiso on 2015/07/22.
+//  Copyright (c) 2015年 祐輔 花田. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SimpleBBSTests/SimpleBBSTests.swift
@@ -0,0 +1,36 @@
+//
+//  SimpleBBSTests.swift
+//  SimpleBBSTests
+//
+//  Created by hiso on 2015/07/22.
+//  Copyright (c) 2015年 祐輔 花田. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SimpleBBSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
